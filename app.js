3eae1f84ca6f39df152eb2637a0734c5055ae562
PrasiddhaBhaktaKhawas <58777982+prasiddhabhaktakhawas@users.noreply.github.com>
2022-06-28 19:27:53 +0545

    globals

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
-const sentence = 'hello world'
-console.log(sentence)
+//__dirname - path to current directory
+//__filename - file name
+//require - function to use modules
+// module - info about current module
+//process - info about environment where the program is being executed
+console.log(__filename)
+setInterval(()=>{
+    console.log("hello world")
+},1000)
