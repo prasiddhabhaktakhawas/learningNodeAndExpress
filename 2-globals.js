d3dc6b54e68039fac0898bab8b1f3df0e7bcd98a
PrasiddhaBhaktaKhawas <58777982+prasiddhabhaktakhawas@users.noreply.github.com>
2022-06-28 19:28:46 +0545

    2-globals

---
+++ 2-globals.js
@@ -0,0 +1,9 @@
+//__dirname - path to current directory
+//__filename - file name
+//require - function to use modules
+// module - info about current module
+//process - info about environment where the program is being executed
+console.log(__filename)
+setInterval(()=>{
+    console.log("hello world")
+},1000)
--- app.js
+++
@@ -1,9 +0,0 @@
-//__dirname - path to current directory
-//__filename - file name
-//require - function to use modules
-// module - info about current module
-//process - info about environment where the program is being executed
-console.log(__filename)
-setInterval(()=>{
-    console.log("hello world")
-},1000)
