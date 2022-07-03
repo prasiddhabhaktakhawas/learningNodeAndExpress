982dd2bb2c1c27f6a165121252d52e8189625a9a
PrasiddhaBhaktaKhawas <58777982+prasiddhabhaktakhawas@users.noreply.github.com>
2022-07-04 02:06:41 +0545

    basic json and params and query

--- 02-express-tutorial/final/06-basic-json.js
+++ 02-express-tutorial/final/06-basic-json.js
@@ -26,25 +26,6 @@ app.get('/api/products/:productID',(req,res)=>{
     return res.json(singleProduct)
 })
 
-app.get('/api/v1/query',(req,res)=>{
-    console.log(req.query)
-    const {search, limit} = req.query
-    let sortedProducts = [...products]
-    if(search){ // http://localhost:5000/api/v1/query?search=a
-        sortedProducts = sortedProducts.filter((product)=>{
-            return product.name.startsWith(search)
-        })
-    }               // http://localhost:5000/api/v1/query?limit=1
-    if(limit){  // http://localhost:5000/api/v1/query?search=a&limit=3
-        sortedProducts = sortedProducts.slice(0, Number(limit)) 
-    }   
-    if(sortedProducts.length<1){ // http://localhost:5000/api/v1/query?search=f&limit=1
-        return res.status(200).send('no products matched') // in case of if statements, we must use return, otherwise, two res.send occurs which yields error. return just closes the function, which is meant to be done after an error
-    }
-    res.status(200).json(sortedProducts)
-    res.send('hello world')
-})
-
 app.get('/api/products/:productID/reviews/:reviewID',(req,res)=>{
     console.log(req.params)
     res.send('hello world')
