6f99749c280fe4942203f1696b069cb0eab1384a
PrasiddhaBhaktaKhawas <58777982+prasiddhabhaktakhawas@users.noreply.github.com>
2022-07-05 01:53:13 +0545

    postman experiments

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -6,9 +6,26 @@ let {people} = require('./data')
 app.use(express.static('./methods-public'))
 // parse form data
 app.use(express.urlencoded({extended:false}))   //built in method of express.It parses incoming requests with urlencoded payloads and is based on body-parser.
+app.use(express.json()) //this is used for javascript form, straight up http . axios has been used
 
 app.get('/api/people', (req,res)=>{
     res.status(200).json({success:true, data:people})
+})
+
+app.post('/api/people', (req, res)=>{
+    const {name} = req.body
+    if(!name){
+        return res.status(400).json({success:false, msg:'please provide name value'})
+    }
+    res.status(201).send({success:true, person:name})
+})
+
+app.post('/api/postman/people', (req,res)=>{
+    const {name} = req.body
+    if(!name){
+        return res.status(400).json({success: false, msg: 'please provide name value'})
+    }
+    res.status(201).send({sucess:true, data: [...people,name]})
 })
 
 app.post('/login',(req,res)=>{  // when form is sumbitted, it is posted, and req has info about what is send, but its encoded. express built in middleware resolves that info and attaches with the request. then we can clearly use req.body
