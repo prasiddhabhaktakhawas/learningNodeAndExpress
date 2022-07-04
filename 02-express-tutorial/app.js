5a8711bd198c135f6847801a770aa811a7d5401a
PrasiddhaBhaktaKhawas <58777982+prasiddhabhaktakhawas@users.noreply.github.com>
2022-07-05 01:16:47 +0545

    simple form posting

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,36 +1,24 @@
 const express = require('express')
 const app = express()
-const morgan = require('morgan')
-const logger = require('./logger')     //imported from logger.js
-const authorize = require('./authorize')
+let {people} = require('./data')
 
-// req => middleware => res
-// app.use(logger)
-// app.use(express.static('./public'))  // just like authorize or logger, static is a built in middleware
-// app.use([logger,authorize]) //we can add path to app.use . So, here /api is path, so it works for both api/products and api/items, but not for / and /about
-// app.use('./api', [logger,authorize])
-app.use(morgan('tiny')) // morgan('tiny') format is basically prebuilt logger middleware
-app.get('/',(req,res)=>{ //immediately goes to logger funtion without printing start and end console logs
-    console.log('start')
-    res.send('home')
-    console.log('end')
+//static assets
+app.use(express.static('./methods-public'))
+// parse form data
+app.use(express.urlencoded({extended:false}))   //built in method of express.It parses incoming requests with urlencoded payloads and is based on body-parser.
+
+app.get('/api/people', (req,res)=>{
+    res.status(200).json({success:true, data:people})
 })
-// app.use(logger)  // in express everything goes in order, since app.get('/') is above app.use, in this case, logger middleware won't work on homepage
-app.get('/about',(req,res)=>{  // one of the way to do it is //app.get('/about',logger,(req,res)=>{   
-    res.send('about')
-})
-app.get('/api/products',(req,res)=>{
-    res.send('products')
-})  
-// app.get('/api/items',[logger,authorize],(req,res)=>{    //if we want to use for only this route, then put [logger,authorize] directly 
-//     console.log(req.user)   
-//     res.send('items')
-// })
-app.get('/api/items',(req,res)=>{    //if we want to use for only this route, then put [logger,authorize] directly 
-    console.log(req.user)   
-    res.send('items')
+
+app.post('/login',(req,res)=>{  // when form is sumbitted, it is posted, and req has info about what is send, but its encoded. express built in middleware resolves that info and attaches with the request. then we can clearly use req.body
+    const {name} = req.body
+    if(name){
+        return res.status(200).send(`welcome ${name}`)
+    }
+    res.status(401).send('please provide credentials')
 })
 
 app.listen(5000, ()=>{
-    console.log('server is listening')
+    console.log('Server is listening on port 5000...')
 })
